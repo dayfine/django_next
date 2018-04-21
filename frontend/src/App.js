1497d49fee71329edbe3353f4078a9c687baedb1
dayfine <difancpa@gmail.com>
2018-04-21 19:40:55 -0400

    use cra

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import YCharts from './YCharts'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <main className='container'>
+          <YCharts />
+        </main>
+      </div>
+    )
+  }
+}
+
+export default App
